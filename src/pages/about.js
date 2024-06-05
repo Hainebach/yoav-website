67e867c5757e57059b2068991bbe80f2405cf471
Hainebach <117458965+Hainebach@users.noreply.github.com>
2024-06-05 13:51:20 +0200

    change titles of sections in the about to be created dynamically

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,6 @@
-import React from "react";
-import { fetchEntries } from "../../lib/contentful";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import { fetchEntries } from "../../lib/contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import ReactMarkdown from "react-markdown";
 import { FaInstagram } from "react-icons/fa";
@@ -17,20 +16,33 @@ export default function About() {
     };
     getInfo();
   }, []);
+
   if (!info) {
     return <div>Loading...</div>;
   }
 
   const { name, about, statement, email, image, cv, instagramLink } =
     info.fields;
+  const sections = {
+    about: documentToReactComponents(about),
+    statement: documentToReactComponents(statement),
+    cv: <ReactMarkdown>{cv}</ReactMarkdown>,
+  };
 
   const toggleSection = (section) => {
     setActiveSection(activeSection === section ? null : section);
   };
 
+  const getTitle = (key) => {
+    if (key === "cv") {
+      return "CV";
+    }
+    return key.charAt(0).toUpperCase() + key.slice(1);
+  };
+
   return (
-    <div className="flex items-start justify-center min-h-screen">
-      <div className="w-full max-w-lg p-8 rounded ">
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="w-full max-w-lg p-8 rounded shadow-md">
         <div className="flex justify-center pb-4">
           <Image
             src={`https:${image.fields.file.url}`}
@@ -38,57 +50,34 @@ export default function About() {
             objectFit="cover"
             width={300}
             height={300}
+            className="rounded"
           />
         </div>
-        <div className="mb-4">
-          <h3
-            className=" font-semibold cursor-pointer"
-            onClick={() => toggleSection("about")}
-          >
-            About
-          </h3>
-          {activeSection === "about" && (
-            <div className="text-sm font-light">
-              {documentToReactComponents(about)}
-            </div>
-          )}
-        </div>
-        <div className="mb-4">
-          <h3
-            className="font-semibold cursor-pointer"
-            onClick={() => toggleSection("statement")}
-          >
-            Statement
-          </h3>
-          {activeSection === "statement" && (
-            <div className="text-sm font-light">
-              {documentToReactComponents(statement)}
-            </div>
-          )}
-        </div>
-        <div className="mb-4">
-          <h3
-            className="font-semibold cursor-pointer"
-            onClick={() => {
-              toggleSection("cv");
-            }}
-          >
-            CV
-          </h3>
-          {activeSection === "cv" && (
-            <ReactMarkdown className="text-sm font-light">{cv}</ReactMarkdown>
-          )}
-        </div>
-        <div className="mt-4 flex justify-center">
-          <a
-            href={instagramLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-gray-900"
-          >
-            <FaInstagram size={30} />
-          </a>
-        </div>
+        {Object.keys(sections).map((key) => (
+          <div className="mb-4" key={key}>
+            <h3
+              className=" font-light hover:font-semibold cursor-pointer"
+              onClick={() => toggleSection(key)}
+            >
+              {getTitle(key)}
+            </h3>
+            {activeSection === key && (
+              <div className="text-sm font-light">{sections[key]}</div>
+            )}
+          </div>
+        ))}
+        {instagramLink && (
+          <div className="mt-4 flex justify-center">
+            <a
+              href={instagramLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-700 hover:text-gray-900"
+            >
+              <FaInstagram size={30} />
+            </a>
+          </div>
+        )}
       </div>
     </div>
   );
